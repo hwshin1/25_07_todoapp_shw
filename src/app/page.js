d8be5a074268e4ef3a13438204664d37904c0031
hwshin1 <hwshin5044@gmail.com>
2025-07-24 12:09:54 +0900

    REACT, prettier 설정 및 mui패키지 추가

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div>안녕 Next.JS</div>
+        항상 함께 해
       </main>
+      <div>
+        대전 너와함께 웃고 너와 함께 울고 항상 함께 해 랄라 라라라라라라 라라라라라 라라라라라
+      </div>
     </div>
   );
 }
